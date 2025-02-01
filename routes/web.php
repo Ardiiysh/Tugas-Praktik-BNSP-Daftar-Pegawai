4f4ded0cc88f7cf21b56f9e40ad743a6b64e9770
Ardi Yansah <ardiyansah@200-100-60-85.dial-up.telesp.net.br>
2025-02-01 22:56:47 +0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PegawaiController;
-
+use Illuminate\Support\Facades\Auth;
 Route::get('/', function () {
     return view('welcome');
 });
